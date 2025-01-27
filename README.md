56fa23c04fe18d018419af37e83d54bb5c04f06e
candice <tinouff@outlook.fr>
2025-01-27 11:48:33 +0100

    ajout projet

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# automates
+# TheorieDesLangages
+
+## Introduction
+
+
+## Les différents fichiers
+
+- `automate.py` : contient la classe `Automate` qui permet de modéliser un automate. Cette contient les méthode permettant de travailler sur un automate.
+- `partie1.py` : contient les tests de la classe `Automate` et les tests des méthodes de la classe `Automate` pour la partie 1 du projet.
+
+
+## Partie 1 : Modélisation d'un automate
+
+
+### 1.1. Modélisation d'un automate
+
+Création d'une classe `Automate` qui permet de modéliser un automate. La classe a les attributs suivants :
+- `etats` : un ensemble d'états
+- `alphabet` : un ensemble de symboles
+- `transitions` : un dictionnaire dont les clés sont des couples (état, symbole) et les valeurs sont des ensembles d'états
+- `etat_initiaux` : un ensemble d'états
+- `etats_finaux` : un ensemble d'états
+
+La classe a les méthodes suivantes :
+
+- `_init_` : le constructeur qui initialise les attributs de la classe
+- `ajouter_etat` : une méthode qui ajoute un état à l'automate
+- `ajouter_transition` : une méthode qui ajoute une transition à l'automate
+-  `destination_transition` : une méthode qui retourne l'ensemble des états atteignables à partir d'un état donné avec un symbole donné
+- `__str__` : une méthode qui retourne une chaîne de caractères représentant l'automate
+
+
+### 1.2. Génération fichier dot et png
+
+- `to_dot` : une méthode de la classe `Automate` qui retourne une chaîne de caractères représentant l'automate au format dot
+- `to_png` : une méthode de la classe `Automate` qui crée un fichier png représentant l'automate
+
+### 1.3 Importation / Export d'un automate
+
+Les méthodes d'importation et d'export sont défini en dehors de la classe `Automate`.
+
+- `exporter_automate` : est une fonction qui permet d'exporter un Automate et prend en paramètre l'automate à exporter et le nom du fichier. 
+La fonction crée un fichier avec le nom donné si celui n'existe pas. Sinon elle modifie le fichier. 
+La structure du fichier d'exportation est la suivante : 
+    - Ligne 1 : Alphabet de l'automate
+    - Ligne 2 : États de l'automate
+    - Ligne 3 : États initiaux 
+    - Ligne 4 : États terminaux
+    - A partir de la ligne 5, nous retrouvons les transition avec la structure suivante : `état départ` `symbols` `état fin`
+
+- `importer_automate` : est une fonction qui permet d'importer un automate depuis un fichier texte et prend en paramètre le nom du fichier texte. 
+La fonction rempli d'abord les variables qui vont nous permettre de créer l'automate. Pour cela elle suit la logique expliqué précédement. 
+Une fois cela effectué, elle crée un nouvel automate qu'elle retourne. 
---
+++ automate.py
@@ -0,0 +1,180 @@
+from graphviz import Digraph
+
+class Automate:
+
+    """
+        La fonction __init__ permet de créer un automate avec un alphabet donné.
+        paramètres:
+            - alphabet: l'alphabet de l'automate
+            - self : l'automate
+    """
+    def __init__(self, alphabet):
+        self.alphabet = alphabet
+        self.etats = set()
+        self.initiaux = set()
+        self.terminaux = set()
+        self.transitions = {}
+
+    """
+        La fonction ajouter_etat permet d'ajouter un état à l'automate.
+        paramètres:
+            - etat: l'état à ajouter
+            - est_initial: indique si l'état est initial
+            - est_terminal: indique si l'état est terminal
+    """
+    def ajouter_etat(self, etat, est_initial=False, est_terminal=False):
+        self.etats.add(etat)
+        if est_initial:
+            self.initiaux.add(etat)
+        if est_terminal:
+            self.terminaux.add(etat)
+
+    """
+        La fonction ajouter_transition permet d'ajouter une transition à l'automate.
+        paramètres:
+            - source: l'état source de la transition
+            - symboles: les symboles de la transition (une liste)
+            - destination: l'état destination de la transition
+    """
+    def ajouter_transition(self, source, symboles, destination):
+        if source not in self.etats or destination not in self.etats:
+            raise ValueError("État source ou destination invalide")
+        for symbole in symboles:
+            if symbole not in self.alphabet:
+                raise ValueError("Symbole non présent dans l'alphabet")
+        if source not in self.transitions:
+            self.transitions[source] = {}
+        if tuple(symboles) in self.transitions[source]:
+            raise ValueError("Transition déjà définie pour ces symboles")
+        self.transitions[source][tuple(symboles)] = destination
+
+    """
+        La fonction destination_transition permet de récupérer l'état destination d'une transition à partir d'un état source et d'un symbole.
+        paramètres:
+            - source: l'état source de la transition
+            - symboles: les symboles de la transition
+    
+        retourne:
+            - l'état destination de la transition si elle existe, None sinon
+    """
+    def destination_transition(self, source, symboles):
+        return self.transitions.get(source, {}).get(tuple(symboles))
+
+    """
+        La fonction __str__ permet d'afficher l'automate.
+        retourne:
+            - une chaîne de caractères représentant l'automate
+    """
+    def __str__(self):
+        result = "Alphabet: " + str(self.alphabet) + "\n"
+        result += "Etats: " + str(self.etats) + "\n"
+        result += "Etats initiaux: " + str(self.initiaux) + "\n"
+        result += "Etats terminaux: " + str(self.terminaux) + "\n"
+        result += "Transitions:\n"
+        for source, transitions in self.transitions.items():
+            for symboles, destination in transitions.items():
+                result += f"     {source} --({', '.join(symboles)})--> {destination}\n"
+        return result
+    
+    """
+        La fonction to_dot permet de générer une représentation graphique de l'automate au format DOT.
+        retourne:
+            - une chaîne de caractères représentant l'automate au format DOT
+    """
+    def to_dot(self):
+        dot = Digraph()
+        dot.attr(rankdir='LR')
+        for etat in self.etats:
+            if etat in self.initiaux:
+                dot.node(etat, shape='point')
+            if etat in self.terminaux:
+                dot.node(etat, shape='doublecircle')
+            dot.node(etat)
+        for source, transitions in self.transitions.items():
+            for symboles, destination in transitions.items():
+                dot.edge(source, destination, label=', '.join(symboles))
+        return dot
+
+    """
+        La fonction to_png permet de générer une représentation graphique de l'automate au format PNG.
+        paramètres:
+            - filename: le nom du fichier PNG à générer
+    """
+    def to_png(self, filename):
+        dot = self.to_dot()
+        dot.render(filename, format='png', cleanup=True)
+
+
+"""
+    La fonction exporter_automate permet d'exporter un automate dans un fichier texte.
+    paramètres:
+        - automate: l'automate à exporter
+        - filename: le nom du fichier
+"""
+def exporter_automate(automate, filename):
+    with open(filename, 'w') as file:
+        # Écriture de l'alphabet
+        file.write(' '.join(automate.alphabet) + '\n')
+        
+        # Écriture des états
+        file.write(' '.join(automate.etats) + '\n')
+        
+        # Écriture des états initiaux
+        file.write(' '.join(automate.initiaux) + '\n')
+        
+        # Écriture des états terminaux
+        file.write(' '.join(automate.terminaux) + '\n')
+        
+        # Écriture des transitions
+        for source, transitions in automate.transitions.items():
+            for symboles, destination in transitions.items():
+                file.write(f"{source} {' '.join(symboles)} {destination}\n")
+
+
+"""
+    La fonction importer_automate permet d'importer un automate à partir d'un fichier texte.
+    paramètres:
+        - filename: le nom du fichier
+    retourne:
+        - l'automate importé
+"""
+def importer_automate(filename):
+    #Vérifie si le fichier existe
+    try:
+        open(filename, 'r')
+    except FileNotFoundError:
+        print("Le fichier n'existe pas")
+        return
+
+    with open(filename, 'r') as file:
+        # Lecture de l'alphabet
+        alphabet = set(file.readline().split())
+        
+        # Lecture des états
+        etats = set(file.readline().split())
+        
+        # Lecture des états initiaux
+        initiaux = set(file.readline().split())
+        
+        # Lecture des états terminaux
+        terminaux = set(file.readline().split())
+        
+        # Création de l'automate
+        automate = Automate(alphabet)
+        for etat in etats:
+            est_initial = etat in initiaux
+            est_terminal = etat in terminaux
+            automate.ajouter_etat(etat, est_initial, est_terminal)
+        
+        # Lecture des transitions
+        for line in file:
+            source, *symboles, destination = line.split()
+            automate.ajouter_transition(source, symboles, destination)
+        
+        return automate
+                
+
+
+
+
+
